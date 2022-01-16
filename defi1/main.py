0a8156c40b89b085cab0d7994d28cc8b9beccfb4
Snow <93426424+Snow667@users.noreply.github.com>
2022-01-16 21:23:25 +0100

    Update main.py

--- defi1/main.py
+++ defi1/main.py
@@ -1,5 +1,17 @@
-# Liste -> 5 INT
-# PLus proche de Input −> print
+"""Votre programme doit prendre 5 entrée différente et donné la plus grande
+(Utilisation de variable de if et de while/for)
+Niveau 2 :
+votre programme prends 5 entrée, il affiche celle qui sont supérieures à une 6e
+(If while/for et un peu de tri)
+Niveau 3:
+Votre programme prend 5 entrée et affiche la plus proche d'une 6e (prévoir le cas d'une égalité) 
+(Du tri plus poussé)
+
+Dans toute défi on utilisera uniquement des nombre entier
+
+Comme d'habitude, le but sera de réaliser le plus rapidement chaque niveau, il y aura un gagnant (qui aura le rôle @winner défi) par niveau.
+Pour les question vous pouvez les poser ici ou me contacter en privé, pour les réponses envoyées moi votre code en privé.
+Le langage de programmation n'est pas imposé"""
 
 valeur_choisi = []
 for i in range(0, 5):
