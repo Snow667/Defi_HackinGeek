be91919c4103d21e94bbaf46f495c4b917581e99
Snow <93426424+Snow667@users.noreply.github.com>
2022-01-16 21:24:03 +0100

    Update main.py

--- defi2/main.py
+++ defi2/main.py
@@ -1,4 +1,14 @@
-from ntpath import join
+"""niveau 1: 
+Compté le nombre d'occurrences d'une lettre choisie dans un mot.
+ex : lettre choisie o, mot bonjour o apparait 2 fois dans bonjour
+niveau 2:
+compté le nombre d'occurrences de chaque lettre d'une phrase puis afficher le résultat dans l'ordre décroissant ex : phrase : "bonjour moi" résultat : o -> 3 b,n,j,u,r,m,i -> 1
+niveau 3 :
+faire un programme permettant de réduire une chaine par rapport au nombre d'occurence de ces lettres ex parce que c'est pas clair:
+chaine : aaaacccttttt résultat 4a3c5t
+Bonus : votre programme peu faire le niveau 3 dans les 2 sens ex : 
+chaine 5d3e7f -> dddddeeefffffff
+Bonne chance !!!!"""
 
 
 mot = input("Ecrivez n'importe quel mot : ")
